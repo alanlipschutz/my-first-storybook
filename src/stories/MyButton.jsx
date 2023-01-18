e9e656f4e7c6c46d88b7c11a13982c89147a21cc
alipschutz-synamedia <alipschutz@synamedia.com>
2023-01-18 17:29:33 +0200

    feat: added css again

--- src/stories/MyButton.jsx
+++ src/stories/MyButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { SvgIcon } from "@mui/material";
-// import '../style.css'
+import '../style.css'
 
 export default function MyButton({
   content,
