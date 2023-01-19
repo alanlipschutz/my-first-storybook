d26eccb1d0f0dcbcf6fc257f50fe2772a3f08fb7
alipschutz-synamedia <alipschutz@synamedia.com>
2023-01-19 16:18:39 +0200

    feat: added styled-components instead of css

--- src/stories/MyButton.jsx
+++ src/stories/MyButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { SvgIcon } from "@mui/material";
-import '../style.css'
+import styled from "styled-components";
 
 export default function MyButton({
   content,
@@ -16,37 +16,48 @@ export default function MyButton({
   iconColor = colorButton,
   iconFontSize = "1rem",
 }) {
-  const style = {
-    backgroundColor: !disabled ? backgroundColor : backgroundDisabled,
-    border: !disabled ? border : "0",
-    borderRadius: "4px",
-  };
+  const Button = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 200px;
+    background-color: ${!disabled ? backgroundColor : backgroundDisabled};
+    border: ${!disabled ? border : "0"};
+    cursor: pointer;
+    padding: ${size === "m" ? "5px 12px" : "3px 12px"};
+    border-radius: 4px;
+    gap: ${size === "m" ? "5px" : "3px"};
+
+    &: disabled {
+      cursor: not-allowed;
+    }
+  `;
+
+  const TextButton = styled.span`
+    font-family: "Source Sans Pro";
+    color: ${!disabled ? colorButton : backgroundDisabled}
+    font-style: normal;
+    font-weight: 600;
+    font-size: ${size === "m" ? "14px" : "12px"};
+    line-height: 20px ${size === "m" ? "20px" : "130%"};
+    display: flex;
+    align-items: center;
+    text-align: center;
+    text-transform: uppercase;
+  `;
 
   const iconStyle = {
     color: iconColor,
     fontSize: iconFontSize,
   };
 
-  const styleButton = {
-    color: !disabled ? colorButton : backgroundDisabled,
-  };
   return (
-    <button
-      onClick={handleClick}
-      style={style}
-      className={`button ${size === "m" ? "button-m" : "button-s"}`}
-      disabled={disabled}
-    >
+    <Button onClick={handleClick} disabled={disabled}>
       {icon ? (
         <SvgIcon style={iconStyle} component={icon} inheritViewBox />
       ) : null}
-      <span
-        style={styleButton}
-        className={size === "m" ? "button-content-m" : "button-content-s"}
-      >
-        {content}
-      </span>
-    </button>
+      <TextButton>{content}</TextButton>
+    </Button>
   );
 }
 
